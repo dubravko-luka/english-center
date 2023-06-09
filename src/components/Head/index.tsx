e8e5bd90ccc7637755ebd9f082a85b713f26ad16
Phuong Nguyen <dev.richard.npm98@gmail.com>
2023-06-09 10:51:24 +0700

    init code

---
+++ src/components/Head/index.tsx
@@ -0,0 +1,72 @@
+import Head from 'next/head';
+import { memo } from 'react';
+
+type Props = {
+	title?: string;
+};
+
+const HeadDocs: React.FC<Props> = ({ title = '' }) => {
+	return (
+		<Head>
+			<title>English Skill</title>
+
+			<meta name="theme-color" content="#ffffff" />
+			<meta name="og:title" content="English Skill" />
+			{/* LOGO */}
+			<meta name="og:image" content="/images/logos/23x23.png" />
+			<meta
+				name="og:description"
+				content="English Skill..."
+			/>
+			<meta
+				name="keywords"
+				content="english, skill, english-skill"
+			/>
+
+			<meta name="renderer" content="webkit" />
+
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="" />
+			<meta name="twitter:title" content="" />
+			{/* LOGO */}
+			<meta name="twitter:image" content="/images/logos/23x23.png" />
+
+			<meta name="title" content="English Skill" />
+			{/* LOGO */}
+			<meta name="image" content="/images/logos/23x23.png" />
+			<meta
+				name="description"
+				content="English Skill..."
+			/>
+
+			<meta name="format-detection" content="email=no" data-shuvi-head="true" />
+			<meta name="apple-mobile-web-app-status-bar-style" content="black" data-shuvi-head="true" />
+			<meta name="apple-mobile-web-app-capable" content="yes" data-shuvi-head="true" />
+			<meta
+				name="keywords"
+				content="english, skill, english-skill"
+				data-shuvi-head="true"
+			/>
+
+			{/* LOGO */}
+			<link rel="apple-touch-icon" type="image/png" href="/images/logos/23x23.png" />
+			<link rel="shortcut icon" href="/images/logos/23x23.png" type="image/x-icon" />
+			<link rel="icon" type="image/png" href="/images/logos/23x23.png" />
+			<link rel="canonical" href="https://english-skill.vercel.app" data-shuvi-head="true" />
+			<link rel="shortcut icon" type="image/x-icon" href="/images/logos/23x23.png" data-shuvi-head="true" />
+
+			<meta property="og:site_name" content="English Skill" data-shuvi-head="true" />
+			<meta property="og:title" content="English Skill" data-shuvi-head="true" />
+			<meta property="og:type" content="website" data-shuvi-head="true" />
+			<meta property="og:url" content="https://english-skill.vercel.app" data-shuvi-head="true" />
+			{/* LOGO */}
+			<meta property="og:image" content="/images/logos/23x23.png" data-shuvi-head="true" />
+			<meta
+				property="og:description"
+				content="English Skill..."
+			/>
+		</Head>
+	);
+};
+
+export default memo(HeadDocs);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import '@/styles/custom.css'
 import type { AppProps } from 'next/app'
 
 import { AppContextProvider } from '@/contexts'
+import Head from '@/components/Head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContextProvider>
+      <Head />
       <Component {...pageProps} />
     </AppContextProvider>
   )
